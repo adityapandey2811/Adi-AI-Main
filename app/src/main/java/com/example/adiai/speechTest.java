609f6e57924f29487c11bbb7a135e8048103d49f
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-07-02 03:18:40 +0530

    Commit start function not completed getLaunchIntentForPackage() error for android 11 still there

--- app/src/main/java/com/example/adiai/speechTest.java
+++ app/src/main/java/com/example/adiai/speechTest.java
@@ -4,9 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.graphics.Color;
 import android.hardware.camera2.CameraManager;
@@ -16,6 +19,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -23,6 +28,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class speechTest extends AppCompatActivity {
     private final int RECOGNIZER_RESULT = 1171;
@@ -38,10 +44,15 @@ public class speechTest extends AppCompatActivity {
                 Vibrator micTouch = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
                 micTouch.vibrate(20);
                 micTouch.vibrate(40);
-                Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speech To Text");
-                startActivityForResult(speechIntent,RECOGNIZER_RESULT);
+                try{
+                    Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speech To Text");
+                    startActivityForResult(speechIntent,RECOGNIZER_RESULT);
+                }
+                catch(Exception ActivityNotFoundException){
+                    Toast.makeText(getApplicationContext(), "Recognizer Error!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -98,12 +109,44 @@ public class speechTest extends AppCompatActivity {
 //        Android 10 does not support wifi automation with program so no need to add it to the code
     }
     private void startFunction() {
-//        try {
-//            Intent launchIntent = new Intent("com.package." + strResult);
-//            startActivity(launchIntent);
-//        } catch (Exception ActivityNotFoundException){
-//            Toast.makeText(getApplicationContext(), "App not found!", Toast.LENGTH_SHORT).show();
+        // Final Code
+//        if(strResult == "android" || strResult == "miui"){
+//            Toast.makeText(getApplicationContext(), strResult + " is OS, Can't open!", Toast.LENGTH_SHORT).show();
 //        }
+//        else if(strResult == "samsung" || strResult == "oneplus" || strResult == "xiaomi" || strResult == "nokia" || strResult == "qualcomm"){
+//            Toast.makeText(getApplicationContext(), strResult + " is Brand, Can't open!", Toast.LENGTH_SHORT).show();
+//        }
+//        else if(strResult == "google"){
+//            strResult = "chrome";
+//        }
+//        else{
+//            final PackageManager pm = getPackageManager();
+////            get a list of installed apps
+//            List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+//            for (ApplicationInfo packageInfo : packages) {
+//                if((packageInfo.packageName).contains(strResult)){
+//                    Toast.makeText(getApplicationContext(), "App Found", Toast.LENGTH_SHORT).show();
+//                    Intent unknownAppIntent = pm.getLaunchIntentForPackage(packageInfo.packageName);
+//                    try {
+//                        startActivity( unknownAppIntent );
+//                    } catch (Exception ActivityNotFoundException){
+//                        Toast.makeText(getApplicationContext(), "Can't open " + strResult, Toast.LENGTH_SHORT).show();
+//                    }
+//                    return;
+//                }
+//            }
+//        }
+        //Final Code over
+        // Helpful
+//        final PackageManager pm = getPackageManager();
+//        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+//
+//        for (ApplicationInfo packageInfo : packages) {
+//            Log.d(null, "Installed package :" + packageInfo.packageName);
+//            Log.d(null, "Source dir : " + packageInfo.sourceDir);
+//            Log.d(null, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+//        }
+
     }
     private void resultParse(){
         if(strResult.contains("turn") && strResult.indexOf("turn") == 0 && strResult.contains("on")){
