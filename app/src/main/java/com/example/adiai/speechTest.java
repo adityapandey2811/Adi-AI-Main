6909557e04167a393ce78df9c6e064f1fffd7457
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-07-01 19:34:51 +0530

    Commit Speech Recognition Added

--- app/src/main/java/com/example/adiai/speechTest.java
+++ app/src/main/java/com/example/adiai/speechTest.java
@@ -1,14 +1,21 @@
 package com.example.adiai;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class speechTest extends AppCompatActivity {
+    private final int RECOGNIZER_RESULT = 1171;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,22 @@ public class speechTest extends AppCompatActivity {
                 Vibrator micTouch = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
                 micTouch.vibrate(20);
                 micTouch.vibrate(40);
+                Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speech To Text");
+                startActivityForResult(speechIntent,RECOGNIZER_RESULT);
             }
         });
+    }
+    private String strResult;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == RECOGNIZER_RESULT && resultCode == RESULT_OK){
+            ArrayList<String> match = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            TextView apiResult = (TextView) findViewById(R.id.results);
+            strResult = match.get(0).toString();
+            apiResult.setText(strResult);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
