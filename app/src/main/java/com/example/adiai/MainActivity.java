3615167acd04e12348a0b05c73282e0de1c42ddd
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-07-01 18:44:19 +0530

    Commit After UI Change

--- app/src/main/java/com/example/adiai/MainActivity.java
+++ app/src/main/java/com/example/adiai/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.adiai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setBackgroundColor(Color.WHITE);
+        btn.setTextColor(Color.BLACK);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/adiai/speechTest.java
@@ -0,0 +1,14 @@
+package com.example.adiai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class speechTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_test);
+    }
+}
